d289d0b9852162af03dd45d3fb059a07982eb251
Prafull Harer <prafullharer@gmail.com>
2026-07-01 02:32:02 +0530

    debug: capture startup errors in api/index.js to diagnose crash

--- api/index.js
+++ api/index.js
@@ -1,2 +1,16 @@
-const app = require('../server/api/index.js');
+let app;
+
+try {
+  app = require('../server/api/index.js');
+} catch (err) {
+  // If the server fails to load, return the actual error instead of crashing
+  app = (req, res) => {
+    res.status(500).json({
+      error: 'Function failed to load',
+      message: err.message,
+      stack: err.stack,
+    });
+  };
+}
+
 module.exports = app;
